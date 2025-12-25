470e029d490518d2cc3863309ea83f2bfa5c1d57
zzhengwei490-dot <zzhengwei490@gmail.com>
2025-12-25 22:02:11 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 告诉它去 app 文件夹里找样式
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
